7dba80e93d09c990253056c1a5ed7638a2cc6e9d
coleherman43 <colesten@gmail.com>
2026-03-23 01:32:19 -0700

    ics to csv converter

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 import pandas as pd
+import csv
+from icalendar import Calendar
+
+# Inspiration: https://medium.com/@bobbie.wxy/convert-calendar-ics-file-to-csv-file-eac0e7c84da0
+def ics_to_csv():
+    # Calendar path vars (create new one for csv)
+    ics_path = 'cole_calendar.ics'
+    csv_path = 'calendar.csv'
+
+    # Write csv to create it
+    csv_file = open(csv_path, 'w', newline='', encoding='utf-8')
+
+    # Create header
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Subject', 'Start Time', 'End Time', 'Description'])
+
+    # Read iCal
+    with open(ics_path, 'rb') as ics_file:
+        cal = Calendar.from_ical(ics_file.read())
+        for event in cal.walk('VEVENT'):
+            subject = event['SUMMARY']
+            start_time = event['DTSTART'].dt
+            if 'DTEND' in event:
+                end_time = event['DTEND'].dt
+            else:
+                end_time = start_time
+            if 'DESCRIPTION' in event:
+                description = event['DESCRIPTION']
+            else:
+                description = None
+            csv_writer.writerow([subject, start_time, end_time, description])
+    csv_file.close()
+
+def import_calendar_data():
+    df = pd.read_csv('calendar.csv')
+
 
 
 def main():
-    # Import data - CLI tool or just file path in code?
-
+    # Convert ics to csv - CLI tool or just file path in code?
+    ics_to_csv()
+    # Import data
+    import_calendar_data()
     # Parse data
 
     # Preprocess data (ex. calculate length)
@@ -12,7 +50,7 @@ def main():
 
     # Optional regex checks for keywords
 
-    # FOR LATER: visualize with streamlit
+    # TODO visualize with streamlit
 
 
     pass
