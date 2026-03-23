fe038c7eb2f17f3b5541ff7d3863d3781b14a501
coleherman43 <colesten@gmail.com>
2026-03-23 00:32:04 -0700

    outline

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def main():
+    # Import data - CLI tool or just file path in code?
+
+    # Parse data
+
+    # Preprocess data (ex. calculate length)
+
+    # Group data by type / aggregate
+
+    # Optional regex checks for keywords
+
+    # FOR LATER: visualize with streamlit
+
+
+    pass
+
+if __name__ == "__main__":
+    main()
